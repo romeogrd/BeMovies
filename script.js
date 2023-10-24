95447a680ad739f3407bc455d1c7c0c03ea3c5f5
romeogrd <romeogerard2@hotmail.fr>
2023-10-24 09:42:43 +0200

    version1

---
+++ script.js
@@ -0,0 +1,30 @@
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: '4',
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+
+
+  /* --------------------------------------------------------------------- */
+
+
+
